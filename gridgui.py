39fa7c18f97ea42a292a165b9718884709fb2f00
Jonathan Duque <johndduque@gmail.com>
2020-06-17 18:44:04 -0400

    basic gui grid system

--- gridgui.py
+++ gridgui.py
@@ -1,1 +1,23 @@
-#Grid Stuff
+import tkinter as tk
+
+def create_grid(event=None):
+    w = c.winfo_width() # Get current width of canvas
+    h = c.winfo_height() # Get current height of canvas
+    c.delete('grid_line') # Will only remove the grid_line
+
+    # Creates all vertical lines at intevals of 100
+    for i in range(0, w, 100):
+        c.create_line([(i, 0), (i, h)], tag='grid_line')
+
+    # Creates all horizontal lines at intevals of 100
+    for i in range(0, h, 100):
+        c.create_line([(0, i), (w, i)], tag='grid_line')
+
+root = tk.Tk()
+
+c = tk.Canvas(root, height=500, width=500, bg='white')
+c.pack(fill=tk.BOTH, expand=True)
+
+c.bind('<Configure>', create_grid)
+
+root.mainloop()
