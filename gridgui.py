7967e456830e5e6e126a22f10f39880ea6554d66
julie.helenek@gmail.com <julie.helenek@gmail.com>
2020-06-19 18:27:11 -0400

    added function that draws points onto canvas

--- gridgui.py
+++ gridgui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter
 
 
 """
@@ -24,6 +25,28 @@ def readCoordsFromCsv(file):
 
     return coordList
 
+#Take values from CSV file and plot them onto the screen
+def pasteCoords(file):
+    coordList = readCoordsFromCsv(file)
+
+    #Take tuples from coordList
+    for coordinate in coordList:
+
+        #Convert tuple to string to get actual values
+        StringCoordinate = str(coordinate)
+
+        #Getting x and y coordinates from tuple string
+        xCoord = StringCoordinate[1:StringCoordinate.index(",")]
+        yCoord = StringCoordinate[StringCoordinate.index(" ")+1:StringCoordinate.index(")")]
+
+        #Convert to floats
+        xCoord = float(xCoord)
+        yCoord = float(yCoord)
+
+        #Create oval based on coordinates
+        c.create_oval(xCoord-10, yCoord-10, xCoord+10 , yCoord+10, fill='red')
+
+
 
 def createGrid(event=None):
     w = c.winfo_width() # Get current width of canvas
@@ -39,11 +62,16 @@ def createGrid(event=None):
         c.create_line([(0, i), (w, i)], tag='grid_line')
 
 
+
+
 root = tk.Tk()
 readCoordsFromCsv("coordinates.txt")
 c = tk.Canvas(root, height=500, width=500, bg='white')
 c.pack(fill=tk.BOTH, expand=True)
 
 c.bind('<Configure>', createGrid)
+pasteCoords("coordinates.txt")
+c.pack(fill =tk.BOTH, expand = True)
+
 
 root.mainloop()
