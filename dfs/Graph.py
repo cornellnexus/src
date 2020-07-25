541e6af731cd246c80082d89a1dc98bb1b921f67
Jonathan Duque <johndduque@gmail.com>
2020-07-25 17:37:00 -0400

    added grid boundaries to graph generation

--- dfs/Graph.py
+++ dfs/Graph.py
@@ -3,8 +3,12 @@ from Coordinate import Coordinate
 coordinates = {}
 
 class Graph:
-    xMax = 3
-    yMax = 3
+    xMax = 10
+    yMax = 10
+
+    def __init__(self, x, y):
+        self.xMax = x
+        self.yMax = y
 
     def performDFSAlgorithm(self,start):
         start.setTraversed(True)
@@ -23,8 +27,10 @@ class Graph:
             return newCoordinateNode
 
     def addNewNeighborNodeToCoordinate(self,coordinateNode,neighborX,neighborY):
-        neighborNode = self.checkIfCoordinateInCoordsDictElseGenerateNode(neighborX,neighborY)
-        coordinateNode.addNeighbor(neighborNode)
+        if (neighborX < self.xMax and neighborX >= 0
+            and neighborY < self.yMax and neighborY > 0):
+            neighborNode = self.checkIfCoordinateInCoordsDictElseGenerateNode(neighborX,neighborY)
+            coordinateNode.addNeighbor(neighborNode)
 
     def createNeighborNodesForCoordinate(self,x,y):
         #we assume that x , y is already created.
@@ -48,62 +54,12 @@ class Graph:
                     coordinates[(x,y)] = Coordinate(x,y,[])
                 self.createNeighborNodesForCoordinate(x,y)
 
-    def printDict(self):
+    def printAllCoordinates(self):
          for key in coordinates.keys():
              print(key, '->', coordinates[key])
 
-g = Graph()
-c1 = Coordinate(1,1,[])
-c2 = Coordinate(1,2,[])
-c3 = Coordinate(1,3,[])
-c4 = Coordinate(1,4,[])
+g = Graph(10,10)
 
-c5 = Coordinate(2,1,[])
-c6 = Coordinate(2,2,[])
-c7 = Coordinate(2,3,[])
-c8 = Coordinate(2,4,[])
-
-c1.addNeighbor(c2)
-c1.addNeighbor(c5)
-c1.addNeighbor(c6)
-
-c2.addNeighbor(c1)
-c2.addNeighbor(c6)
-c2.addNeighbor(c3)
-c2.addNeighbor(c5)
-c2.addNeighbor(c7)
-
-c3.addNeighbor(c2)
-c3.addNeighbor(c4)
-c3.addNeighbor(c7)
-c3.addNeighbor(c8)
-c3.addNeighbor(c6)
-
-c4.addNeighbor(c3)
-c4.addNeighbor(c8)
-c4.addNeighbor(c7)
-
-c5.addNeighbor(c1)
-c5.addNeighbor(c2)
-c5.addNeighbor(c6)
-
-c6.addNeighbor(c5)
-c6.addNeighbor(c7)
-c6.addNeighbor(c2)
-c6.addNeighbor(c1)
-c6.addNeighbor(c3)
-
-c7.addNeighbor(c6)
-c7.addNeighbor(c8)
-c7.addNeighbor(c3)
-c7.addNeighbor(c2)
-c7.addNeighbor(c4)
-
-c8.addNeighbor(c7)
-c8.addNeighbor(c4)
-c8.addNeighbor(c3)
-
-#g.performDFSAlgorithm(c1)
 g.generateGraph()
 
 g.performDFSAlgorithm(coordinates[(1,1)])
