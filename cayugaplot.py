67be5ef419d99ff39250e89f3d4942fd70f90fe8
Nina Tang <nt285@cornell.edu>
2020-08-22 10:31:05 -0400

    updated cayugaplot.py with the contents of nina5.py

--- cayugaplot.py
+++ cayugaplot.py
@@ -2,32 +2,26 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-
-def write_coords(long, lat):
-    f = open("cayugacoords.txt", "a")
-    f.write("\n"+str(long)+","+str(lat))
-    f.close()
-
-
-def plot_point(long, lat):
-    ax.scatter(long, lat, zorder=1, alpha=1, c='b', s=10)
-    write_coords(long, lat)
-    plt.show()
-
-
-df = pd.read_csv("cayugacoords.txt")
-BBox = [-76.5119, -76.5013, 42.4596, 42.4642]
+#df = pd.read_csv("cayugacoords.txt")
+BoundaryBox = [-76.5119, -76.5013, 42.4596, 42.4642]
 ruh_m = plt.imread('map.png')
 fig, ax = plt.subplots(figsize=(8, 7))
 
-ax.scatter(df.longitude, df.latitude, zorder=1, alpha=1, c='r', s=10)
 ax.set_title('Cayuga Lake Shore')
-ax.set_xlim(BBox[0], BBox[1])
-ax.set_ylim(BBox[2], BBox[3])
-ax.imshow(ruh_m, zorder=0, extent=BBox, aspect='equal')
+ax.set_xlim(BoundaryBox[0], BoundaryBox[1])
+ax.set_ylim(BoundaryBox[2], BoundaryBox[3])
+ax.imshow(ruh_m, zorder=0, extent=BoundaryBox, aspect='equal')
 
+
+def animate(nframe):
+    ax.collections = []
+    df = pd.read_csv("cayugacoords.txt")
+    points = ax.scatter(df.longitude, df.latitude, zorder=1,
+                        alpha=1, c='r', s=10)
+    return
+
+
+anim = animation.FuncAnimation(fig, animate)
 plt.show()
-
-plot_point(-76.5105, 42.462)
-plot_point(-76.5105, 42.4621)
