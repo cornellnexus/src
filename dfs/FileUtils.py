d86ce7102eea88291cb4038abf207c012d48d584
julie.helenek@gmail.com <julie.helenek@gmail.com>
2020-08-27 14:17:08 -0400

    Added longitude,latitude to top of file when run

--- dfs/FileUtils.py
+++ dfs/FileUtils.py
@@ -4,6 +4,9 @@ def writeCoordsToCSVFile(coords):
     csv_file = open("DFS.txt", "w+", newline='')
     csv_writer = csv.writer(csv_file)
 
+    longitude = "longitude"
+    latitude = "latitude"
+    csv_writer.writerow([longitude,latitude])
     for (x,y) in coords:
         csv_writer.writerow([x,y])
 
