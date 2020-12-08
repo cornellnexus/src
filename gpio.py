0de1f9c81ad37e57489ba6bed72fc594e2fc01ef
Luis Martinez <louisvizard@Luiss-MacBook-Pro.local>
2020-12-08 15:45:32 -0800

    working out pwm stuff. Added comments

--- gpio.py
+++ gpio.py
@@ -29,8 +29,7 @@ def setup():
     #Motor Driver 
     GPIO.setup([in1, in2, in3, in4], GPIO.OUT, initial=GPIO.LOW) #In1, In2, In3, In4
     GPIO.setup([enA, enB], GPIO.OUT) #EnA, EnB
-    GPIO.PWM(enA, 1000)    # create object D2A for PWM on port 25 at 1KHz
-    GPIO.PWM(enB, 1000)
+
     #IMU
     GPIO.setup(imu_tx, GPIO.OUT)
     GPIO.setup(imu,rx, GPIO.IN)
@@ -49,19 +48,53 @@ def goForward():
     GPIO.output([in1, in4],GPIO.LOW)
     GPIO.output([in2, In3], GPIO.HIGH)
 
+    """ Need to restructure code. Pwm objects go out of scope after function ends, so pwm
+    will stop when function stops. Could make this file a class and have p1, p2 as member variables
+    in the object so that their scope is global. Also, constructor would handle all the setup. 
+    p1 = GPIO.PWM(enA, 1000) #1000Hz (Arduino Uno is 976)
+    p2 = GPIO.PWM(enB, 1000) #1000Hz (Arduino Uno is 976)
+
+    p1.start(78) #78% on. Equivalent to 200/255 in arduino
+    p2.start(78) #78% on. Equivalent to 200/255 in arduino
+    """
+
 def reverse():
     GPIO.output([in2, In3], GPIO.LOW)
     GPIO.output([in1, in4],GPIO.HIGH)
+
+    """
+    p1 = GPIO.PWM(enA, 1000) #1000Hz (Arduino Uno is 976)
+    p2 = GPIO.PWM(enB, 1000) #1000Hz (Arduino Uno is 976)
+
+    p1.start(78) #78% on. Equivalent to 200/255 in arduino
+    p2.start(78) #78% on. Equivalent to 200/255 in arduino
+    """
 
 def turnLeft():
     GPIO.output([in1, in2, in4],GPIO.LOW)
     GPIO.output([in3], GPIO.LOW)
     #analogWrite equivalent in rPi
 
+    """
+    p1 = GPIO.PWM(enA, 1000) #1000Hz (Arduino Uno is 976)
+    p2 = GPIO.PWM(enB, 1000) #1000Hz (Arduino Uno is 976)
+
+    p1.start(59) #59% on. Equivalent to 150/255 in arduino
+    p2.start(98) #98% on. Equivalent to 250/255 in arduino
+    """
+
 def turnRight():
     GPIO.output([in1, in3, in4],GPIO.LOW)
     GPIO.output([in2], GPIO.LOW)
     #analogWrite equivalent in rPi
+
+    """
+    p1 = GPIO.PWM(enA, 1000) #1000Hz (Arduino Uno is 976)
+    p2 = GPIO.PWM(enB, 1000) #1000Hz (Arduino Uno is 976)
+
+    p1.start(98) #98% on. Equivalent to 250/255 in arduino
+    p2.start(59) #59% on. Equivalent to 150/255 in arduino
+    """
 
 def stop():
     GPIO.output([in1, in2, in3, in4],GPIO.LOW)
@@ -81,4 +114,4 @@ while true:
     #duration = pulseIn(echoPin, HIGH); //times sound wave length
     distance = (duration * 0.0343)/2; #0.0343 = speed of sound
     # print(distance)
-    sleep(0.001);
+    sleep(0.001)
