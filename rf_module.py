d6aaedfd44bfff4fb868ec911e46ce252b92e421
Luis Martinez <lmm343@cornell.edu>
2020-12-30 22:33:03 -0800

    Update rf_module.py

--- rf_module.py
+++ rf_module.py
@@ -17,4 +17,14 @@ while True:
 """
 def write_data(inp_string):
     ser.write(inp_string.encode()) #need to turn input string into byte array for some reason
+    
+def read_data():
+    try:
+        while True: 
+            data = ser.readline()
+            print(data)
+            time.sleep(0.2)
+    except KeyboardInterrupt: 
+        print("Press Ctrl-C to terminate while statement")
+        pass
 """
