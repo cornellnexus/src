9add356db8c9a6e20bde903836ff928be6a0bc88
Jessica Yuan <jjy37@cornell.edu>
2021-01-11 13:18:42 -0800

    Restructured code so more readable and added comment about changing derivative branch

--- pid_controller.py
+++ pid_controller.py
@@ -43,8 +43,17 @@ while True:
     e1_error = TARGET - e1.value
     e2_error = TARGET - e2.value
 
-    m1_speed += (e1_error * KP) + ((e1_error - e1_prev_error)/ SAMPLETIME) * KD + (e1_sum_error * KI)
-    m2_speed += (e2_error * KP) + ((e2_error - e2_prev_error)/ SAMPLETIME) * KD  + (e2_sum_error * KI)
+
+    # Potential Fix: the integral branch doesn't seem to be taking integral, it's just summing up the errors. I think the example code simplifies it to sum of error instead of actually taking integral
+    # e1_integral = e1_integral_prior + e1_error * SAMPLETIME
+    # e2_integral = e2_integral_prior + e2_error * SAMPLETIME
+    
+    e1_derivative = (e1_error - e1_prev_error)/ SAMPLETIME
+    e2_derivative = (e2_error - e2_prev_error)/ SAMPLETIME
+
+
+    m1_speed += (e1_error * KP) + e1_derivative * KD + (e1_sum_error * KI)
+    m2_speed += (e2_error * KP) + e2_derivative * KD  + (e2_sum_error * KI)
 
     m1_speed = max(min(1, m1_speed), 0)
     m2_speed = max(min(1, m2_speed), 0)
@@ -52,13 +61,13 @@ while True:
     r.value = (m1_speed, m2_speed)
 
     print("e1 {} e2 {}".format(e1.value, e2.value))
-    print("m1 {} m2 {}".format(m1_speed, m2_speed))    
-    
+    print("m1 {} m2 {}".format(m1_speed, m2_speed))
+
     e1.reset()
     e2.reset()
     sleep(SAMPLETIME)
-    
+
     e1_prev_error = e1_error
-    e2_prev_error = e2_error    
+    e2_prev_error = e2_error
     e1_sum_error += e1_error
     e2_sum_error += e2_error
