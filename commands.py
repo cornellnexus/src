c2a7b018ba948d6ec8bb49af59696edb76524b6a
Yagmur Dulger <yd333@cornell.edu>
2021-03-20 12:33:16 -0400

    print statements in commands

--- commands.py
+++ commands.py
@@ -21,39 +21,45 @@ e2.start(100)
 
 # stops the robot 
 def stop():
-    GPIO.output([in1, in2, in3, in4],GPIO.LOW)
+    #GPIO.output([in1, in2, in3, in4],GPIO.LOW)
+    print('stop')
 
 # moves the robot forward 
 def go_forward():
-    GPIO.output([in1, in4],GPIO.LOW)
-    GPIO.output([in2, in3], GPIO.HIGH)
+    # GPIO.output([in1, in4],GPIO.LOW)
+    # GPIO.output([in2, in3], GPIO.HIGH)
+    print('go_forward')
 
 # reverses the robot 
 def reverse():
-    GPIO.output([in2, in3], GPIO.LOW)
-    GPIO.output([in1, in4],GPIO.HIGH)
+    # GPIO.output([in2, in3], GPIO.LOW)
+    # GPIO.output([in1, in4],GPIO.HIGH)
+    print('reverse')
 
 # turns the robot left for 1 second 
 def turn_left():
-    GPIO.output([in2, in4],GPIO.LOW)
-    GPIO.output([in1, in3], GPIO.HIGH)
+    # GPIO.output([in2, in4],GPIO.LOW)
+    # GPIO.output([in1, in3], GPIO.HIGH)
+    print('turn_left')
     time.sleep(1)
 
 # turns the robot right for 1 second 
 def turn_right():
-    GPIO.output([in1, in3],GPIO.LOW)
-    GPIO.output([in2, in4], GPIO.HIGH)
+    # GPIO.output([in1, in3],GPIO.LOW)
+    # GPIO.output([in2, in4], GPIO.HIGH)
+    print('turn_right')
     time.sleep(1)
 
 # pauses the robot for 0.2 seconds
 def pause():
-    GPIO.output([in1, in2, in3, in4], GPIO.LOW) #In1, In2, In3, In4
+    # GPIO.output([in1, in2, in3, in4], GPIO.LOW) #In1, In2, In3, In4
+    print('pause')
     time.sleep(0.2)
 
 # testing the functions to run under 10 seconds 
-# stopTime = time.time() + 5
-# while time.time() < stopTime:
-#     go_forward()
+stopTime = time.time() + 5
+while time.time() < stopTime:
+    go_forward()
 # # 
 # stop()
 # 
