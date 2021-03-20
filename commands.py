d98c0ec97e86ca84c44ac1bf375a319e3e750656
Yagmur Dulger <yd333@cornell.edu>
2021-03-20 12:28:56 -0400

    testing commands

--- commands.py
+++ commands.py
@@ -51,9 +51,9 @@ def pause():
     time.sleep(0.2)
 
 # testing the functions to run under 10 seconds 
-stopTime = time.time() + 5
-while time.time() < stopTime:
-    go_forward()
+# stopTime = time.time() + 5
+# while time.time() < stopTime:
+#     go_forward()
 # # 
 # stop()
 # 
