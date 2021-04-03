5ab7a2dfc6e0c2f9af41f5953de39e74def244e1
Angela Loh <angelaloh09@gmail.com>
2021-04-03 16:34:10 -0400

    while GGA not found, continue reading serial line

--- gps.py
+++ gps.py
@@ -11,37 +11,28 @@ ser = serial.Serial('/dev/ttyACM0', 19200, timeout = 5)
 csv_data = []
 collect_time = time.time() + 60
 
-def parse_gps(str):
-    if str.find('GGA') > 0: 
-        msg = pynmea2.parse(str)
-        data = (msg.longitude, msg.latitude)
-        return(data)
+def parse_gps(serial_line):
+    msg = pynmea2.parse(serial_line)
+    data = (msg.longitude, msg.latitude)
+    return(data)
 
 def update_step():
     line = str(ser.readline())
     decoded_line = line[2:-5]
-#     print(decoded_line)
     if len(decoded_line) == 0: 
         print("Time out! exit. \n")
         sys.exit()
+    while gps_line.find('GGA') < 0: 
+        print("no GGA -- gps_line: ",gps_line)
+        line = str(ser.readline())
     coord = parse_gps(decoded_line)
-    if isinstance(coord, tuple):
-        #csv_data.append(coord)
-        print('----------------GOT COORD!!!!!----------------')
-        print('is tuple: ' + str(coord))
+    if isinstance(coord, tuple): 
+        print("tuple coord: ", coord)
         return(coord)
+    # if isinstance(coord, tuple):
+    #     print('----------------GOT COORD!!!!!----------------')
+    #     print('is tuple: ' + str(coord))
+    #     return(coord)
     print('not tuple: '+str(coord))
-        
 
-def write_to_csv(csv_data):
-    while time.time() < collect_time:
-        update_step()
-    with open('gps_noise.csv','w') as gps_file:
-        for coord in csv_data:
-            gps_file.write(str(coord) + '\n')
-            
-#write_to_csv(csv_data)
-
-# while True: 
-#     write_to_csv(csv_data)
- 
+update_step()
