fb69cb39c66a667cd92988c843c8228239714b44
angelaloh <angelaloh09@gmail.com>
2021-04-07 22:00:59 -0400

    elimated none values -- test outside

--- gps.py
+++ gps.py
@@ -8,54 +8,27 @@ import csv
 from gpio import *
 
 ser = serial.Serial('/dev/ttyACM0', 19200, timeout = 5)
-csv_data = []
-collect_time = time.time() + 60
-# CODE does not work atm. error. 
-def gps():
-    gps_line = ser.readline()
-    if len(gps_line)==0:
-        print("exiting. timeout")
-        sys.exit()
-    while gps_line.find('GGA')<0:
-        print("re-reading")
-        gps_line = ser.readline()
-    gps_data = pynmea2.parse(gps_line)
-    my_lat = float(gps_data.latitude)
-    my_lon = float(gps_data.longitude)
-    print((my_lon, my_lat))
-    print("----------------------")
-    return(my_lon, my_lat)
 
-while(True):
-    gps()
-    delay(2)
+""" parse_gps[gps_line]: takes in serial gps data and returns
+    a coordinate in the form (longitude, latitude).
+    precondition: gps_line must be a string of pynmea format.
+"""
+def parse_gps(gps_line):
+    msg = pynmea2.parse(gps_line)
+    data = (msg.longitude, msg.latitude)
+    return(data)
 
-
-# def parse_gps(serial_line):
-#     msg = pynmea2.parse(serial_line)
-#     print(msg)
-#     data = (msg.longitude, msg.latitude)
-#     return(data)
-
-# def update_step():
-#     line = str(ser.readline())
-#     decoded_line = line[2:-5]
-#     if len(decoded_line) == 0: 
-#         print("Time out! exit. \n")
-#         sys.exit()
-#     while line.find('GGA') < 0: 
-#         print("no GGA -- gps_line: ",line)
-#         line = str(ser.readline())
-#     coord = parse_gps(decoded_line)
-#     if isinstance(coord, tuple): 
-#         print("tuple coord: ", coord)
-#         return(coord)
-    # if isinstance(coord, tuple):
-    #     print('----------------GOT COORD!!!!!----------------')
-    #     print('is tuple: ' + str(coord))
-    #     return(coord)
-#     print('not tuple: '+str(coord))
-# 
-# while(True):
-#     update_step()
-#     delay(2)
+""" update_step: returns the tuple (long, lat).
+    note: if the string 'GGA' is not found in the serial data, update_step is called again.
+"""
+def update_step():
+    gps_line = str(ser.readline())
+    decoded_line = gps_line[2:-5]
+    flag = decoded_line.find('GGA')
+    if flag < 0:
+        print("No GGA")
+        update_step()
+    else:
+        coord = parse_gps(decoded_line)
+        print("coord: ", coord)
+        return(coord)
