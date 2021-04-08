7d56c9f053b9ab5089d781c3e0ee754b83944e87
Angela Loh <angelaloh09@gmail.com>
2021-04-08 13:51:29 -0400

    delete sys

--- gps.py
+++ gps.py
@@ -1,26 +1,30 @@
 """ Module that includes functions for GPS sensor  """
 
-import os, sys
 import serial
 import time
 import pynmea2
 import csv
 from gpio import *
 
-ser = serial.Serial('/dev/ttyACM0', 19200, timeout = 5)
+ser = serial.Serial('/dev/ttyACM0', 19200, timeout=5)
 
 """ parse_gps[gps_line]: takes in serial gps data and returns
     a coordinate in the form (longitude, latitude).
     precondition: gps_line must be a string of pynmea format.
 """
+
+
 def parse_gps(gps_line):
     msg = pynmea2.parse(gps_line)
     data = (msg.longitude, msg.latitude)
     return(data)
 
+
 """ update_step: returns the tuple (long, lat).
     note: if the string 'GGA' is not found in the serial data, update_step is called again.
 """
+
+
 def update_step():
     gps_line = str(ser.readline())
     decoded_line = gps_line[2:-5]
