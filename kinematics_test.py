6a1b93a5efdc63735769993e7def2d4171867be1
Samantha Zhang <sjz46@cornell.edu>
2021-07-07 20:15:08 -0400

    structure for kinetmatics_test

--- kinematics_test.py
+++ kinematics_test.py
@@ -5,13 +5,39 @@ import unittest
 Unit tests for kinematics.py
 '''
 
-class TestRobotCoordsConversions(unittest.TestCase):
+class TestRobotGlobalConversions(unittest.TestCase):
   def test_robot_to_global(self):
+    print("not used, what is global")
+  def test_global_to_robot(self):
+    print("not used, what is global")
+
+class TestAlegbra(unittest.TestCase):
+  def test_feedback_lin(self):
+    print("test")
+  def test_limit_cmds(self):
+    print("test")
+  def test_integrate_odom(self):
     print("test")
 
-# class TestMeterConversions(unittest.TestCase):
+class TestMeterConversions(unittest.TestCase):
+  def test_meters_to_gps(self):
+    print("test")
+  def test_meters_to_lat(self):
+    print("test")
+    # self.assertEqual(0.1, meters_to_lat(185)) 
+  def test_meters_to_long(self):
+    print("test")
 
-# class TestVincentyConversions(unittest.TestCase):
+class TestVincentyConversions(unittest.TestCase):
+  def test_get_vincenty_x(self):
+    print("test")
+  def test_get_vincenty_y(self):
+    print("test")
+  def test_get_haversine_x(self):
+    print("test")
+  def test_get_haversine_y(self):
+    print("test")
+
 
 # class TestHaversineConversions(unittest.TestCase):
 
