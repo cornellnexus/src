db2489aacdb23dc17a996c32297772b4202be937
Julie Helenek <julie.helenek@gmail.com>
2021-08-05 20:16:10 -0400

    added robot test to test runner

--- compilation_tests/test_runner.py
+++ compilation_tests/test_runner.py
@@ -3,6 +3,7 @@ import compilation_tests.node_test as node_test
 import compilation_tests.sim_traj_test as sim_traj_test
 import compilation_tests.pid_controller_test as pid_controller_test 
 import compilation_tests.state_manager_test as state_manager_test 
+import compilation_tests.robot_test as robot_test
 
 '''
 Runs all compilation test files as individual modules
@@ -12,7 +13,7 @@ loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 #adding tests: 
-compilation_tests = [node_test, sim_traj_test, pid_controller_test, state_manager_test]
+compilation_tests = [node_test, sim_traj_test, pid_controller_test, state_manager_test, robot_test]
 for test_case in compilation_tests: 
     suite.addTests(loader.loadTestsFromModule(test_case))
 
