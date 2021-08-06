1d49dfe4a8d8ee10c8d7d106bdc66232e60b7b05
Julie Helenek <julie.helenek@gmail.com>
2021-08-05 20:17:48 -0400

    added kinematics test to test_runner

--- compilation_tests/test_runner.py
+++ compilation_tests/test_runner.py
@@ -4,6 +4,7 @@ import compilation_tests.sim_traj_test as sim_traj_test
 import compilation_tests.pid_controller_test as pid_controller_test 
 import compilation_tests.state_manager_test as state_manager_test 
 import compilation_tests.robot_test as robot_test
+import compilation_tests.kinematics_test as kinematics_test
 
 '''
 Runs all compilation test files as individual modules
@@ -13,7 +14,7 @@ loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 #adding tests: 
-compilation_tests = [node_test, sim_traj_test, pid_controller_test, state_manager_test, robot_test]
+compilation_tests = [node_test, sim_traj_test, pid_controller_test, state_manager_test, robot_test, kinematics_test]
 for test_case in compilation_tests: 
     suite.addTests(loader.loadTestsFromModule(test_case))
 
