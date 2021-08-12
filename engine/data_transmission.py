a73e09022404080dda3c1b92dd13cc33e1656b4f
Angela Loh <angelaloh09@gmail.com>
2021-08-11 18:21:41 -0700

    data transmission layout

---
+++ engine/data_transmission.py
@@ -0,0 +1,10 @@
+"""
+1) get data from software 
+- 
+2) get data from hardware 
+
+3) place into dictionary data structure (every cycle)
+
+4) change into json / other thing that is faster to transmit 
+* note: if value / variable is not updated each cycle, then send previous value
+"""
