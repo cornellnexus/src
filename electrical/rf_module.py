25293bc8665706cfa12bc4fbae51930976e5f76d
Angela Loh <angelaloh09@gmail.com>
2021-09-14 18:35:47 -0400

    added rf_module class and starting methods

--- electrical/rf_module.py
+++ electrical/rf_module.py
@@ -1,59 +1,35 @@
+import serial 
+import time 
+
 """ This module receives the serially transmitted data from the rf module"""
+class RadioSession:
+    def __init__(self, port='/dev/ttyS0', device = 0):
+        self.ser = serial.Serial('/dev/ttyS0', 57600)
+        self.baudrate = 57600
+        self.port = port
+        self.connect = False
+        self.device_list = [0, 1, 2] #to do: figure out better way to do this
+        self.device = device
 
-import sys
-import time
-import serial
-import difflib
-from gpio import *
+    def receive_data(self):
+        byte_data = self.ser.readline()
+        return byte_data
 
-tx = rf_tx
-rx = rf_rx
+    def transmit_data(self,t_data):
+        cast_data = bytes((str(t_data) + '\n'), encoding= 'utf-8')
+        self.ser.write(cast_data)
 
-# Ground Station 
-ser_gnd = serial.Serial(port="computer port", 9600, timeout=0)
-
-
-def gnd_receive():
-    while ser_gnd.inWaiting():
-        received_data_gnd = ser_gnd.readline()
-        print(received_data)
-        time.sleep(0.2)
-
-
-def gnd_transmit(data):
-    ser_gnd.write(data)
-
-
-# RPi Station
-
-def rpi_receive():
-    '''
-    input data to rpi by wrapping gpio pins as uart port.
-    '''
-    try:
-        pi = pigpio.pi()
-        pi.set_mode(rx, pigpio.INPUT)
-        pi.bb_serial_read_open(rx, 9600, 8)
-
-        print("data - software serial: ")
-        while True:
-            (count, rf_data) = pi.bb_serial_read(rx)
-            if count:
-                print
-                rf_data
-        time.sleep(1)
-
-except:
-pi.bb_serial_read_close(rx)
-pi.stop()
-
-# ser_rpi = serial.Serial(port = "rpi port", 9600, timeout = 0)
-
-# def rpi_receive(): 
-#   while ser_rpi.inWaiting():  
-#       received_data_rpi = ser_rpi.readline()
-#       print(received_data)
-#       time.sleep(0.2)
-
-# def rpi_transmit(data):
-#   ser_rpi.write(data)
+    def startup(self): 
+        pass
+        """
+        device 0 sends data to device 1
+        device 1 reads the data from device 0
+        device 1 confirms data received by sending back to 0
+        device 0 confirms receives data from device 1
+        device 0 sends required data to device 1
+        Idea for handshakes:
+        state machine with states:
+        initialization- establish radio connection through three way handshake, checksums/
+            rate we send data
+        the rest- start constand transmition/recive, checksums
+        """
