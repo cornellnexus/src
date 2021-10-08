b399bc6489eaa233f16fd0eb3e0f173082a597d1
Jonathan Lee <jonathanslee4@gmail.com>
2021-10-08 15:11:12 -0400

    Live data and pre-collected data options are now available

--- engine/heading_visualizer.py
+++ engine/heading_visualizer.py
@@ -8,26 +8,31 @@ from engine.sensor_module import SensorModule
 
 if __name__ == "__main__":
 
+    # Determine data read mode
+    IS_LIVE_DATA = False
+    live_data = int(input("1: Visualize heading using live data from an IMU\n2: Visualize heading using pre-collected "
+                          "data stored in a CSV file\nEnter your choice: "))
+    if live_data != 1 and live_data != 2:
+        raise Exception("Invalid input.")
+    IS_LIVE_DATA = live_data == 1
+
     # Plot setup
     fig = plt.figure()
     fig.set_dpi(100)
     fig.set_size_inches(7, 6.5)
-
     ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
-
     circle_patch = plt.Circle((0, 0), 1, fc="green")
     wedge_patch = patch.Wedge(
-        (0, 0), 3, 100, 80, animated=True, fill=False, width=2, ec="g", hatch="xx"
-    )
+        (0, 0), 3, 100, 80, animated=True, fill=False, width=2, ec="g", hatch="xx")
 
-    # Sensor setup
-    # sensor_module = SensorModule()
-    with open("csv/imu_360_sample1.csv") as file:
-        lines = file.readlines()
-        # lines = [line.rstrip() for line in lines]
-        lines = [ast.literal_eval(line) for line in lines]
+    # Data reading setup
+    if IS_LIVE_DATA:
+        sensor_module = SensorModule()
+    else:
+        with open("csv/imu_360_sample1.csv") as file:
+            imu_readings = file.readlines()
+            imu_readings = [ast.literal_eval(line) for line in imu_readings]
 
-    print(lines)
 
     def init():
         circle_patch.center = (0, 0)
@@ -37,16 +42,18 @@ if __name__ == "__main__":
 
 
     def animate(i):
-        # sensor_module.update_imu_data()
+        if IS_LIVE_DATA:
+            sensor_module.update_imu_data()
+            middle_heading = math.degrees(math.atan2(sensor_module.imu_dict["mag_y"], sensor_module.imu_dict["mag_x"]))
+        else:
+            middle_heading = math.degrees(math.atan2(imu_readings[i]["mag"]["y"], imu_readings[i]["mag"]["x"]))
 
-        middle_heading = math.degrees(math.atan2(lines[i]["mag"]["y"], lines[i]["mag"]["x"]))
         wedge_patch.update({"center": [0, 0]})
         wedge_patch.theta1 = middle_heading - 10
         wedge_patch.theta2 = middle_heading + 10
         # time.sleep(0.1)
-        print(i)
         return circle_patch, wedge_patch
 
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(lines)-1, interval=20, blit=True)
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(imu_readings) - 1, interval=20, blit=True)
     plt.show()
