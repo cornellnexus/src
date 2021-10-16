b2ef85556f39ac47dd161d751b285f8e75f7e73c
Julie Helenek <julie.helenek@gmail.com>
2021-10-16 15:49:51 -0400

    updating test_runner with new tests

--- compilation_tests/test_runner.py
+++ compilation_tests/test_runner.py
@@ -1,10 +1,10 @@
 import unittest
 import compilation_tests.node_test as node_test
-import compilation_tests.sim_traj_test as sim_traj_test
 import compilation_tests.pid_controller_test as pid_controller_test
 import compilation_tests.state_manager_test as state_manager_test
 import compilation_tests.robot_test as robot_test
 import compilation_tests.kinematics_test as kinematics_test
+import compilation_tests.traversal_test as traversal_test
 
 '''
 Runs all compilation test files as individual modules
@@ -20,7 +20,7 @@ loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # adding tests:
-compilation_tests = [node_test, pid_controller_test, state_manager_test, robot_test, kinematics_test]
+compilation_tests = [node_test, pid_controller_test, state_manager_test, robot_test, kinematics_test, traversal_test]
 for test_case in compilation_tests:
     suite.addTests(loader.loadTestsFromModule(test_case))
 
