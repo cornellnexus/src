7e5cc2483dbcc0a63946842981f7eb4554221ec5
Julie Helenek <julie.helenek@gmail.com>
2021-10-16 16:24:01 -0400

    updating tests

--- compilation_tests/traversal_test.py
+++ compilation_tests/traversal_test.py
@@ -34,6 +34,16 @@ class TestTraversalFunctions(unittest.TestCase):
         unvisited_waypoints = r2d2.execute_traversal(waypoints_to_visit, allowed_dist_error)
         self.assertEqual(deque([]), unvisited_waypoints)
 
+    def test_different_allowed_dist(self):
+        r2d2 = Robot(42, -76, math.pi / 2, epsilon=0.2, max_v=0.5, radius=0.2, init_phase=2)
+        allowed_dist_error = 0.65
+        grid=Grid(42.444250, 42.444599, -76.483682, -76.483276)
+        grid_mode="borders"
+        all_waypoints = grid.get_waypoints(grid_mode)
+        waypoints_to_visit = deque(all_waypoints)
+        unvisited_waypoints = r2d2.execute_traversal(waypoints_to_visit, allowed_dist_error)
+        self.assertEqual(deque([]), unvisited_waypoints)
+
     def test_different_heading(self):
         r2d2 = Robot(42, -76, math.pi, epsilon=0.2, max_v=0.5, radius=0.2, init_phase=2)
         allowed_dist_error = 0.5
@@ -44,7 +54,7 @@ class TestTraversalFunctions(unittest.TestCase):
         unvisited_waypoints = r2d2.execute_traversal(waypoints_to_visit, allowed_dist_error)
         self.assertEqual(deque([]), unvisited_waypoints)
 
-    def test_with_minimal_nose(self):
+    def test_with_minimal_noise(self):
         r2d2 = Robot(42, -76, math.pi, epsilon=0.2, max_v=0.5, radius=0.2, init_phase=2, position_noise=0.05)
         allowed_dist_error = 0.5
         grid=Grid(42.444250, 42.444599, -76.483682, -76.483276)
