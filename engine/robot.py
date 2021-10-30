6ac28715eec0d754ca662f34be0baf3e0a1af8e0
Allen Chen <allenchen799@gmail.com>
2021-10-30 18:42:14 -0400

    Update robot.py
    recommited roomba movement

--- engine/robot.py
+++ engine/robot.py
@@ -44,7 +44,7 @@ class Robot:
 
     def __init__(self, x_pos, y_pos, heading, epsilon, max_v, radius, is_sim=True, position_kp=1, position_ki=0,
                  position_kd=0, position_noise=0, heading_kp=1, heading_ki=0, heading_kd=0, heading_noise=0,
-                 init_phase=1, time_step=0.1):
+                 init_phase=1, time_step=0.1, control_mode=1):
         """
         Arguments:
             x_pos: the x position of the robot, where (0,0) is the bottom left corner of the grid with which
@@ -84,6 +84,7 @@ class Robot:
         self.heading_ki = heading_ki
         self.heading_kd = heading_kd
         self.heading_noise = heading_noise
+        self.control_mode = Control_Mode(control_mode)
 
         self.loc_pid_x = PID(
             Kp=self.position_kp, Ki=self.position_ki, Kd=self.position_kd, target=0, sample_time=self.time_step,
@@ -207,8 +208,14 @@ class Robot:
     def execute_setup(self):
         pass
 
-    def execute_traversal(self, unvisited_waypoints, allowed_dist_error):
+    def execute_traversal(self, unvisited_waypoints, allowed_dist_error, grid, control_mode, time_limit):
+        match control_mode:
+            case Control_Mode.LAWNMOVER:
+                self.lawn_mover(unvisited_waypoints, allowed_dist_error)
+            case Control_Mode.ROOMBA:
+                self.roomba_mover(grid, time_limit)
 
+    def lawn_mover(self, unvisited_waypoints, allowed_dist_error):        
         while unvisited_waypoints:
             curr_waypoint = unvisited_waypoints[0].get_m_coords()
             self.move_to_target_node(curr_waypoint, allowed_dist_error)  # TODO: add obstacle avoidance support
@@ -216,6 +223,30 @@ class Robot:
 
         self.phase = Phase.RETURN
         return unvisited_waypoints
+
+    def roomba_mover(self, grid, time_limit):
+        # move the robot in a roomba-like manner
+        move_dist = 3  # how much the robot moves in meter
+        turn_dist = .5233  # how much the robot turns in rads
+        dt = 0
+        exit_boolean = False # battery_limit, time_limit, battery_capacity is full
+
+        while not exit_boolean:
+            curr_x = self.state[0]
+            curr_y = self.state[1]
+            # lowerleft (lower bound) of the grid is 0,0. upperright bound would be difference between max and min
+            roomba_out_of_x_bound = curr_x < 0 or curr_x > (grid.long_max-grid.long_min)
+            roomba_out_of_y_bound = curr_y < 0 or curr_y > (grid.lat_max-grid.lat_min)
+
+            if roomba_out_of_x_bound or roomba_out_of_y_bound:
+                self.move_forward(-move_dist)
+                self.turn(turn_dist)
+            else:
+                self.move_forward(move_dist)
+            dt += dt
+            exit_boolean = (dt > time_limit)
+        self.phase = Phase.COMPLETE
+        return None
 
     def execute_avoid_obstacle(self):
         pass
