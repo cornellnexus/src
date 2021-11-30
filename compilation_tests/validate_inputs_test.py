709e25f792ea8e9e3abeab1822f4ab586c3afd50
Angela Loh <angelaloh09@gmail.com>
2021-11-29 23:19:09 -0500

    adding compilation tests

---
+++ compilation_tests/validate_inputs_test.py
@@ -0,0 +1,19 @@
+from gui.validate_inputs import *
+import unittest
+
+'''
+Data parsing test for GUI's Input Validation
+'''
+
+class ValidateInputTest(unittest.TestCase):
+    def test_get_phase(self):
+        pass
+
+    def test_get_median(self):
+        pass
+
+    def test_fix_data_size(self): 
+        pass 
+
+    def test_build_validated_packet(self): 
+        pass 
