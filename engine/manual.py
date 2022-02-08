d63e3f4b34028d53cb3c519faf81b176fda0f329
Allen Chen <ac2324@cornell.edu>
2022-02-08 18:08:32 -0500

    recommitting todos and fixing robot_data.csv

--- engine/manual.py
+++ engine/manual.py
@@ -10,6 +10,7 @@ class Manual():
     def __init__(self, control_mode):
         self.control_mode = ControlMode(control_mode)
 
+    # commented part of the code is reading serially. not commented part is testing via csv files
     def get_control_mode(self):
         robot_command_file = open((self.get_path('csv')[-1] + '/control_mode_test.csv'), "r+")
         # byte_data = ser.readline()
@@ -18,12 +19,16 @@ class Manual():
         last_line = last_line.strip()
         control_mode = last_line[last_line.index(".") + 1:len(last_line)]
         if control_mode == 'Manual':
-            return ControlMode.MANUAL
+            self.control_mode = ControlMode(Manual)
+        # problem right now is that gui sends only "manual" or "autonomous" so robot_data is where we contain the
+        # control modes. However, robot data is a csv file, which is something on the gui side that the robot
+        # will not have access to.
         elif control_mode == 'Autonomous':
-            return self.control_mode
+            self.control_mode = ControlMode()
 
     def execute_manual(self):
         robot_data_file = open((self.get_path('csv')[-1] + '/robot_command.csv'), "r+")  # open csv file of robot data
+        # instead of rereading serialized data (which I don't know works), save the data in a field of Manual and pull from there.
         # byte_data = ser.readline()
         # data = byte_data.decode()
         datas = robot_data_file.readlines()
@@ -40,7 +45,7 @@ class Manual():
         robot_data_file.seek(0)
         robot_data_file.truncate()
 
-    def get_path(self, folder):
+    def get_path(self, folder): #only useful for testing. Remove when reading file serially.
 
         cwd = os.getcwd()
         sys.path.append(cwd + "/" + folder)
