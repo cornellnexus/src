a02ccb3fd633816f33f0917ed2058d58b1aa676a
Samantha Zhang <sjz46@cornell.edu>
2022-02-19 10:42:17 -0500

    Revert "merged master"
    This reverts commit fea0a2b66d6b2c23c0c17f8d4aecaca71ad81196, reversing
    changes made to d63e3f4b34028d53cb3c519faf81b176fda0f329.

--- gui/together.py
+++ gui/together.py
@@ -1,11 +1,8 @@
 import os
 #run using <python -m gui.together>
 
-
+os.system("python -m engine.sim_trajectory &")
 os.system("python -m gui.gui &")
-os.system("python -m gui.retrieve_inputs &")
-os.system("python -m engine.sim_trajectory")
-
 
 
 
