013ec7d7cfbc52941618ed2aff6be31f7d3cdb68
Angela Loh <angelaloh09@gmail.com>
2022-03-26 13:24:51 -0400

    division in testing

--- compilation_tests/test_runner.py
+++ compilation_tests/test_runner.py
@@ -1,18 +1,25 @@
 import unittest
 # import compilation_tests.astar_test as astar_test
+
 import compilation_tests.database_test as database_test
-import compilation_tests.gps_test as gps_test
 import compilation_tests.grid_test as grid_test
-import compilation_tests.imu_test as imu_test
-import compilation_tests.kinematics_test as kinematics_test # Fixed
 import compilation_tests.mag_heading_test as mag_heading_test 
 import compilation_tests.movement_test as movement_test
 import compilation_tests.node_test as node_test #TODO: FIX NODE TEST
 import compilation_tests.packet_test as packet_test
-import compilation_tests.pid_controller_test as pid_controller_test #fixed
 import compilation_tests.retrieve_inputs_test as retrieve_inputs_test
-import compilation_tests.robot_test as robot_test #TODO: FIX ROBOT_TEST
+
+
+
+import compilation_tests.gps_test as gps_test
+import compilation_tests.imu_test as imu_test
+import compilation_tests.pid_controller_test as pid_controller_test #fixed
+import compilation_tests.kinematics_test as kinematics_test # Fixed
 import compilation_tests.traverse_standard_test as traverse_standard_test # Fixed
+import compilation_tests.robot_test as robot_test #TODO: FIX ROBOT_TEST
+
+
+
 
 '''
 Runs all compilation test files as individual modules
