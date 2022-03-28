f55b4829ce83cf44032b667b4f465f6c51a2618d
Angela Loh <angelaloh09@gmail.com>
2022-03-27 22:47:40 -0400

    fixed node test

--- tests/compilation_tests/test_runner.py
+++ tests/compilation_tests/test_runner.py
@@ -1,8 +1,4 @@
 import unittest
-# import compilation_tests.astar_test as astar_test
-# import compilation_tests.database_test as database_test
-# import compilation_tests.node_test as node_test #TODO: FIX NODE TEST
-
 
 import tests.compilation_tests.pid_controller_test as pid_controller_test #fixed
 import tests.compilation_tests.kinematics_test as kinematics_test # Fixed
@@ -13,17 +9,14 @@ import tests.compilation_tests.packet_test as packet_test #fixed
 import tests.compilation_tests.retrieve_inputs_test as retrieve_inputs_test #fixed
 import tests.compilation_tests.mag_heading_test as mag_heading_test #fixed
 import tests.compilation_tests.grid_test as grid_test #fixed
+import tests.compilation_tests.node_test as node_test #fixed
+import compilation_tests.database_test as database_test #TODO
+
 
 
 
 '''
 Runs all compilation test files as individual modules
-
-Compilation test files DO NOT INCLUDE: 
--gps_test
--imu_test
--mag_heading_test
--movement_test 
 '''
 
 loader = unittest.TestLoader()
@@ -31,7 +24,7 @@ suite = unittest.TestSuite()
 
 # adding tests:
 compilation_tests = [pid_controller_test, kinematics_test, traverse_standard_test, execute_setup_test, 
-robot_test, packet_test, retrieve_inputs_test, mag_heading_test, grid_test]
+robot_test, packet_test, retrieve_inputs_test, mag_heading_test, grid_test, node_test]
 
 for test_case in compilation_tests:
     suite.addTests(loader.loadTestsFromModule(test_case))
