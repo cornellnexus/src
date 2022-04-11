85040a8044a1b4f8452dddb47ea893961304272f
Angela Loh <angelaloh09@gmail.com>
2022-04-11 13:38:29 -0400

    testing framework

---
+++ tests/functionality_tests/gui_to_rpi.py
@@ -0,0 +1,11 @@
+# Testing Procedure: 
+
+# 1. Initialize Radio modules (raspberry pi + computer). Ensure that data can be transmitted & received properly. 
+
+# 2. Initialize GUI running at the same time  
+
+# 3. Begin transmitting data from the raspberry pi’s radio module (read packets from a gui_to_rpi.csv) 
+
+#   TODO: need to figure out when we want data to be transmitted in the code
+
+# 4. Check if the GUI has updated as expected. 
