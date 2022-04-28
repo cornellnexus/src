f41aaffd5e5ddc3abb6b16847c39203147a1ca3f
Shreya1233 <shreyak500@gmail.com>
2022-04-28 17:39:54 -0400

    packet testing

--- tests/functionality_tests/gui_to_rpi.py
+++ tests/functionality_tests/gui_to_rpi.py
@@ -16,14 +16,17 @@ def get_path(folder):
 # 2. Initialize GUI running at the same time  
 
 # 3. Begin transmitting data from the raspberry pi’s radio module (read packets from a gui_to_rpi.csv) 
-ser = serial.Serial("/dev/cu.usbserial-017543DC", 57600)
-rpi_to_gui = open((get_path('csv')[-1] + '/gui_to_rpi.csv'), "r")  # open csv file of rpi to gui data
+ser = serial.Serial("/dev/ttyS0", 57600)
+rpi_to_gui = open((get_path('tests')[-1] + '/functionality_tests/csv/gui_to_rpi.csv'), "r")  # open csv file of rpi to gui data
 
 while True:
-  packet = rpi_to_gui.readlines()[0]  # basic packet
-  # packet = rpi_to_gui.readlines()[-1]  # get last line of csv file
+  packet = "phase:0;p_weight:0.0;acc:0.01,0.01,0.01;n_dist:00.0;rot:00.00;last_n:000.00,000.00;vel:0.00;next_n:000.00,000.00;coord:000.00,000.00;batt:000;ctrl:1" + "\n"
+  # basic packet
+#   packet = str(rpi_to_gui.readlines()[-1]) + "\n" # get last line of csv file
+
+  cast_data = bytes(packet, encoding = 'utf-8') 
   print("Sending the following packet: " + str(packet))
-  ser.writelines(packet)
+  ser.write(cast_data)
 #   TODO: need to figure out when we want data to be transmitted in the code
 
 # Now try in sim trajectory - uncomment # RPI_GUI_TEST lines in sim trajectory
