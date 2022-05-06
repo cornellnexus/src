40facf95eab56aef835cb89de2c32c4a31934d08
sazhang02 <sjz46@cornell.edu>
2022-05-05 22:36:29 -0400

    rpi script for rpi to gui connection

--- tests/functionality_tests/gui_to_rpi.py
+++ tests/functionality_tests/gui_to_rpi.py
@@ -9,8 +9,7 @@ def get_path(folder):
     sys.path.append(cwd + "/" + folder)
     return sys.path
 
-# Testing Procedure: 
-
+# Testing Procedure:
 # 1. Initialize Radio modules (raspberry pi + computer). Ensure that data can be transmitted & received properly. 
 
 # 2. Initialize GUI running at the same time  
@@ -20,14 +19,11 @@ ser = serial.Serial("/dev/ttyS0", 57600)
 rpi_to_gui = open((get_path('tests')[-1] + '/functionality_tests/csv/gui_to_rpi.csv'), "r")  # open csv file of rpi to gui data
 
 while True:
-  packet = "phase:1;p_weight:1.0;acc:0.01,0.01,0.01;n_dist:00.0;rot:00.00;last_n:000.00,000.00;vel:0.00;next_n:000.00,000.00;coord:000.00,000.00;batt:000;ctrl:1" + "\n"
-  # basic packet
-#   packet = str(rpi_to_gui.readlines()[-1]) + "\n" # get last line of csv file
-
+  #packet = "phase:3;p_weight:02.0;acc:3.01,4.01,5.01;n_dist:60.0;rot:07.00;last_n:008.00,008.00;vel:9.00;next_n:010.00,011.00;coord:030.00,015.00;batt:013;ctrl:3" + "\n"
+  packet = "phase:4;p_weight:02.0;acc:0.00,4.00,2.00;n_dist:40.0;rot:02.00;last_n:008.00,008.00;vel:9.00;next_n:010.00,011.00;coord:030.00,015.00;batt:013;ctrl:3" + "\n"
   cast_data = bytes(packet, encoding = 'utf-8') 
   print("Sending the following packet: " + str(packet))
   ser.write(cast_data)
-#   TODO: need to figure out when we want data to be transmitted in the code
 
 # Now try in sim trajectory - uncomment # RPI_GUI_TEST lines in sim trajectory
 
