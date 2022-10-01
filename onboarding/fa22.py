ec128ddbdedbfb4ea8e3d78945ddbe1b0950e073
Samantha Zhang <sjz46@cornell.edu>
2022-10-01 02:44:03 -0400

    create onboarding demo

---
+++ onboarding/fa22.py
@@ -0,0 +1,6 @@
+print("Welcome to Nexus!! For our onboarding, we will practice using Git and Github!")
+
+names = [] # Add your name here and push your changes!
+
+for name in names:
+  print("Welcome " + name)
