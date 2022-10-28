b3fee59d534d6810b878e31f95bd00bba1176543
Angela Loh <angelaloh09@gmail.com>
2022-10-28 12:42:32 -0400

    test

--- engine/robot.py
+++ engine/robot.py
@@ -2,11 +2,11 @@ import numpy as np
 import math
 from engine.kinematics import integrate_odom, feedback_lin, limit_cmds
 from engine.pid_controller import PID
-# from electrical.motor_controller import MotorController
-# import electrical.gps as GPS 
-# import electrical.imu as IMU 
-# import electrical.radio_module as RadioSession
-# import serial
+from electrical.motor_controller import MotorController
+import electrical.gps as GPS 
+import electrical.imu as IMU 
+import electrical.radio_module as RadioSession
+import serial
 
 from constants.definitions import CSV_PATH
 
@@ -104,11 +104,11 @@ class Robot:
         self.gyro_rotation = [0, 0, 0]  # TEMPORARY
         self.linear_v = 0
         self.angular_v = 0
-        if not self.is_sim:
-            self.motor_controller = MotorController(self, wheel_radius = 0, vm_load1 = 1, vm_load2 = 1, L = 0, R = 0)
-            self.robot_radio_session = RadioSession(serial.Serial('/dev/ttyS0', 57600)) 
-            self.gps = GPS(serial.Serial('/dev/ttyACM0', 19200, timeout=5)) 
-            self.imu = IMU(busio.I2C(board.SCL, board.SDA)) 
+        # if not self.is_sim:
+        #     self.motor_controller = MotorController(self, wheel_radius = 0, vm_load1 = 1, vm_load2 = 1, L = 0, R = 0)
+        #     self.robot_radio_session = RadioSession(serial.Serial('/dev/ttyS0', 57600)) 
+        #     self.gps = GPS(serial.Serial('/dev/ttyACM0', 19200, timeout=5)) 
+        #     self.imu = IMU(busio.I2C(board.SCL, board.SDA)) 
 
         self.loc_pid_x = PID(
             Kp=self.position_kp, Ki=self.position_ki, Kd=self.position_kd, target=0, sample_time=self.time_step,
