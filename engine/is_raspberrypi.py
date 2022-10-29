7391362584d9766c3d0c67b4c079a7ce38e09352
Lin <lj233@cornell.edu>
2022-10-29 13:20:08 -0400

    add specification

--- engine/is_raspberrypi.py
+++ engine/is_raspberrypi.py
@@ -1,6 +1,7 @@
 import io
 import os
-"""a function from : https://raspberrypi.stackexchange.com/questions/5100/detect-that-a-python-program-is-running-on-the-pi/74541#74541"""
+"""a function from : https://raspberrypi.stackexchange.com/questions/5100/detect-that-a-python-program-is-running-on-the-pi/74541#74541
+return True if the current device is a raspberry_pi, False otherwise"""
 def is_raspberrypi():
     try:
         with io.open('/sys/firmware/devicetree/base/model', 'r') as m:
