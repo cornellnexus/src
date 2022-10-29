102eb74ef6d9ba886f11c3bdda787216a1a667f3
Dhriti Verma <dhritiverma02@gmail.com>
2022-10-29 15:36:55 -0400

    logic works but gives an input output error - debugging not complete

--- electrical/lidar_sensor.py
+++ electrical/lidar_sensor.py
@@ -11,15 +11,18 @@ class Lidar:
         self.ser = serial.Serial("/dev/serial0", 115200,timeout=0) 
     
     def get_lidar_data(self):
-        counter = self.ser.in_waiting # count the number of bytes of the serial port
-        if counter > 8:
-            bytes_serial = self.ser.read(9) # read 9 bytes
-            self.ser.reset_input_buffer() # reset buffer
+        while True:
+            counter = self.ser.in_waiting # count the number of bytes of the serial port
+            if counter > 8:
+                bytes_serial = self.ser.read(9) # read 9 bytes
+                self.ser.reset_input_buffer() # reset buffer
+            
+                #print('Distance = ', bytes_serial[2]+bytes_serial[3]*256, ' -------- ', end = '')
 
-        if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59: # check first two bytes
-            distance = bytes_serial[2] + bytes_serial[3]*256 # distance in next two bytes
-            strength = bytes_serial[4] + bytes_serial[5]*256 # signal strength in next two bytes
-            return distance/100.0,strength
+                if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59: # check first two bytes
+                    distance = bytes_serial[2] + bytes_serial[3]*256 # distance in next two bytes
+                    strength = bytes_serial[4] + bytes_serial[5]*256 # signal strength in next two bytes
+                    return distance
 
 
     def check_data_reliability(self, strength):
@@ -43,28 +46,39 @@ class Lidar:
         '''
         Detects if the bin is full or not
         '''
+        print('checking if full')
         init_time = time.time()
         counter = 0
         while time.time() < init_time + sample_rate*10:
-            new_obj_dist = self.get_lidar_data()
-            if counter > 10*sample_rate:
-                return True #object detected 
-            if new_obj_dist < bucket_dist:
-                counter+=1
+                print('reading taken', counter)
+            #try:
+                new_obj_dist = self.get_lidar_data()
+                if counter > 10*sample_rate:
+                    return True #object detected 
+                if new_obj_dist <= bucket_dist:
+                    counter+=1
+                else:
+                    return False #false alarm
+            #except:
+                #print('THE EXCEPT IS HERE')
+                #break
 
     
 ## testing code: 
 if __name__ == "__main__":
-    lidar= Lidar()
+    lidar = Lidar()
     sample_rate = 100 
     lidar.set_samp_rate(sample_rate) #configuring the sensor: sample rate and default baud rate 11000smth
     bucket_dist = 100 #in cm
     while True:
-        try:        
-            distance,strength = lidar.get_lidar_data() # read values
-            print('Distance: {0:2.2f} m, Strength: {1:2.0f} / 65535 (16-bit)'.format(distance,strength))
-            if distance < bucket_dist:
+        #try:        
+            distance = lidar.get_lidar_data() # read values
+            print("Distance: ", distance)
+            if distance <= bucket_dist:
                 bucket_full = lidar.check_if_full(sample_rate, bucket_dist)
                 print("bucket is full: ", bucket_full)
-        except:
-            print("no data")
+                if bucket_full == True:
+                    break
+        #except:
+            #print("error")
+            #break
