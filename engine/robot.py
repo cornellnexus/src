690c48b39f780dcd277d3aea8dc3883d76f403cb
Samantha Zhang <sjz46@cornell.edu>
2022-11-02 18:11:46 -0400

    restore timestep

--- engine/robot.py
+++ engine/robot.py
@@ -51,7 +51,7 @@ class Robot:
 
     def __init__(self, x_pos, y_pos, heading, epsilon, max_v, radius, is_sim=True, position_kp=1, position_ki=0,
                  position_kd=0, position_noise=0, heading_kp=1, heading_ki=0, heading_kd=0, heading_noise=0,
-                 init_phase=1, time_step=.1, move_dist=.5, turn_angle=3, plastic_weight=0, use_ekf=False,
+                 init_phase=1, time_step=1, move_dist=.5, turn_angle=3, plastic_weight=0, use_ekf=False,
                  init_gps=(0, 0), gps_data=(0, 0), imu_data=None, ekf_var=None, gps=None, imu=None, motor_controller=None):
         """
         Arguments:
