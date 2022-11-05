c37eef83981b175efc8b4de887c1df565b81bc3a
Lin <lj233@cornell.edu>
2022-11-05 13:30:14 -0400

    refactor get_all_lawnmower_waypoints_adjustable

--- engine/grid.py
+++ engine/grid.py
@@ -370,59 +370,37 @@ class Grid:
         waypoints = []
         waypoints.append(curr_pos)
         while (phase != WaypointPhase.TERMINATE):
-            if (phase == WaypointPhase.DOWN):
-                if direction == Direction.LEFT:
-                    new_pos = (curr_pos[0]-1,curr_pos[1])
-                    if curr_pos[0]-1 == 0 and self.nodes[new_pos].is_active:
-                       
-                        waypoints.append(new_pos)
-                        left_pos = self.bottom_leftmost_node(new_pos)
-                        if left_pos is not None:
-                            waypoints.append(left_pos)
-                            curr_pos = left_pos
-                            phase = WaypointPhase.DOWN
-                        else:
-                            phase = WaypointPhase.TERMINATE
-                    elif self.nodes[new_pos].is_active:
-                        print("branch 2 ",new_pos)
-                        waypoints.append(new_pos)
-                        curr_pos = new_pos
+            if direction == Direction.LEFT:
+                new_pos = (curr_pos[0]-1,curr_pos[1])
+                # next_next_pos = (curr_pos[0]-2,curr_pos[1])
+                if self.nodes[new_pos].is_active:
+                    print("branch 2 ",new_pos)
+                    waypoints.append(new_pos)
+                    curr_pos = new_pos
+                else:
+                    print("branch 3",new_pos)
+                    left_pos = self.bottom_rightmost_node(new_pos)
+                    if left_pos is not None:
+                        waypoints.append(left_pos)
+                        curr_pos = left_pos
+                        direction = Direction.RIGHT
                     else:
-                        print("branch 3",new_pos)
-                        left_pos = self.bottom_rightmost_node(new_pos)
-                        if left_pos is not None:
-                            waypoints.append(left_pos)
-                            curr_pos = left_pos
-                            phase = WaypointPhase.DOWN
-                            direction = Direction.RIGHT
-                        else:
-                            phase = WaypointPhase.TERMINATE
-                if direction == Direction.RIGHT:
-                    new_pos = (curr_pos[0]+1,curr_pos[1])
-                    if curr_pos[0]+1 == rows and self.nodes[new_pos].is_active:
-                        print("branch 1", new_pos)
-                        waypoints.append(new_pos)
-                        right_pos = self.bottom_rightmost_node(new_pos)
-                        if right_pos is not None:
-                            waypoints.append(right_pos)
-                            curr_pos = right_pos
-                            phase = WaypointPhase.DOWN
-                        else:
-                            phase = WaypointPhase.TERMINATE
-                    elif self.nodes[new_pos].is_active:
-                        print("branch 2", new_pos)
-                        waypoints.append(new_pos)
-                        curr_pos = new_pos
+                        phase = WaypointPhase.TERMINATE
+            if direction == Direction.RIGHT:
+                new_pos = (curr_pos[0]+1,curr_pos[1])
+                if self.nodes[new_pos].is_active:
+                    print("branch 2", new_pos)
+                    waypoints.append(new_pos)
+                    curr_pos = new_pos
+                else:
+                    print("branch 3", new_pos)
+                    right_pos = self.bottom_leftmost_node(new_pos)
+                    if right_pos is not None:
+                        waypoints.append(right_pos)
+                        curr_pos = right_pos
+                        direction = Direction.LEFT
                     else:
-                        print("branch 3", new_pos)
-                        right_pos = self.bottom_leftmost_node(new_pos)
-                        if right_pos is not None:
-                            waypoints.append(right_pos)
-                            curr_pos = right_pos
-                            phase = WaypointPhase.DOWN
-                            direction = Direction.LEFT
-                        else:
-                            phase = WaypointPhase.TERMINATE
+                        phase = WaypointPhase.TERMINATE
         return waypoints
 
 
