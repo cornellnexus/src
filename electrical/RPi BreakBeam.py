5c827c0c1f80b98e15bb2848d184eae907600386
AmberL235 <79765187+AmberL235@users.noreply.github.com>
2023-03-09 12:10:52 -0500

    Update RPi BreakBeam.py

--- electrical/RPi BreakBeam.py
+++ electrical/RPi BreakBeam.py
@@ -5,15 +5,20 @@ beam_broken = False
 
 def break_beam_callback(channel):
     if GPIO.input(BEAM_PIN):
-        print("beam unbroken")
+        beam_broken = False
+        print(beam_broken)
     else:
         beam_broken = True
-        print("beam broken")
+        print(beam_broken)
     return beam_broken
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(BEAM_PIN, GPIO.BOTH, callback=break_beam_callback)
+def check_break_beam():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(BEAM_PIN, GPIO.BOTH, callback=break_beam_callback)
 
-message = input("Press enter to quit\n\n")
-GPIO.cleanup()
+    message = input("Press enter to quit\n\n")
+    GPIO.cleanup()
+    return break_beam_callback
+
+check_break_beam()
