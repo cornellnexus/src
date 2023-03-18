60eb0ec31edfba466c5ac486055f0d5ff577d690
AdityaKompella6 <adipkompella@gmail.com>
2023-03-18 01:17:25 -0400

    Fixed up the docking script so it looks nice.

--- april_tag_updates/docking_script.py
+++ april_tag_updates/docking_script.py
@@ -1,12 +1,11 @@
-expected_dist = 35+2
+expected_dist = 37
 known_width = 4.18 ##inches
 focal_length = 1.37478492e+03
 from electrical.motor_controller import BasicMotorController
-import imutils
 import numpy as np
 import cv2
 import time 
-from engine.robot import Robot
+from enum import Enum
 ARUCO_DICT = {
 	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
 	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
@@ -35,15 +34,14 @@ arucoParams = cv2.aruco.DetectorParameters_create()
 cap = cv2.VideoCapture(0)
 window = 'Camera'
 cv2.namedWindow(window)
-# image = cv2.imread("april_test.jpg")
 def distance_to_camera(knownWidth, focalLength, perWidth):
   # compute and return the distance from the maker to the camera
   return (knownWidth * focalLength) / perWidth
 
 def direction_of_tag(corner,image):
-  (topLeft, topRight, bottomRight, bottomLeft) = corner
-  h,w = image.shape[0], image.shape[1]
-  cx, cy = int(960),int(540)
+  (_, _, _, _) = corner
+  _,w = image.shape[0], image.shape[1]
+  cx, _ = int(960),int(540)
   if  bottomLeft[0] > cx:
     return "right"
   else:
@@ -54,17 +52,16 @@ CACHED_PTS = None
 CACHED_IDS = None
 Line_Pts = None
 measure = None
-idDict = {0: "R1", 2: "R2", 3: "R3"}
+idDict = {1: "R1", 2: "R2", 3: "R3"}#TODO add all the other tags
 visited = []
 timeout =  10
 angle_zero = 10
-step1_done = False
-step2_done = False
-step3_done = False 
-step4_done = False 
-#once robot_state is created, we want to delete robot and just do motor = BasicMotorController()
-robot = Robot(x_pos = 0, y_pos = 0, heading = 0, epsilon = 0, max_v = 0, radius = 1)
-motor = BasicMotorController(robot)
+class Steps_Done(Enum):
+    one = False
+    two = False
+    three = False
+    four = False
+motor = BasicMotorController(True)
 start_time = None
 while True:
     if start_time is None:
@@ -72,49 +69,46 @@ while True:
     ret, image = cap.read()
     (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
     if len(corners) == 0:
-      if time.time() - start_time > timeout and step1_done is False:
+      if time.time() - start_time > timeout and Steps_Done.one is False:
         #Step 1 - print("Check for april tags rn by rotating in circle for 15 seconds.")
           motor.turn_right()
-      if step1_done is True and step2_done is False:
-        # Step 2 - print(go back pls until len(corners) = 1)
+      if Steps_Done.one and Steps_Done.two is False:
+        #Step 2 - print(go back pls until len(corners) = 1)
         motor.reverse()
-        step2_done = True
     if len(corners) > 0:
+      if Steps_Done.one:
+        Steps_Done.two = True
       start_time = time.time()
       for (corner, markerId) in zip(corners, ids):
           # print(direction_of_tag(corner[0],image))
           markerId = float(markerId)
           (topLeft, topRight, bottomRight, bottomLeft) = corner[0]
           pixel_width = np.sqrt((bottomRight[0] - bottomLeft[0])**2 + (bottomRight[1] - bottomLeft[1])**2)
-          depth_distance = distance_to_camera(known_width,focal_length,pixel_width)
+          depth = distance_to_camera(known_width,focal_length,pixel_width)
           # print(f"{depth_distance} in")
           cxt,cyt = int((topLeft[0] + bottomRight[0])//2),int((topLeft[1] + bottomRight[1])//2)
           cx, cy = int(960),int(540)
           cv2.circle(image, (cxt, cyt), radius = 20, color = (0, 0, 255), thickness = -1)
           cv2.circle(image, (cx, cy), radius = 20, color = (0, 0, 255), thickness = -1)
           cv2.line(image,(cxt, cyt),(cx, cy), color = (0, 0, 0), thickness = 2)
-          # print(cxt, cyt)
           ratio= known_width/pixel_width
-          # print(ratio)
           dpix = np.sqrt((cx-cxt)**2+(cy-cyt)**2)
           horiz_dist = dpix * ratio
           midpoint = (int((cx + cxt) //2 ), int((cy + cyt) //2 - 25 ))
           midpoint2 = (int((cx + cxt) //2), int((cy + cyt) //2 + 50 ))
           midpoint3 = (int((cx + cxt) //2), int((cy + cyt) //2 - 100 ))
           cv2.putText(image, f"{horiz_dist: .2f} in", midpoint, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 101, 255), 2)
-          # print(d_in)
-          angle = np.degrees(np.arcsin(horiz_dist/(depth_distance)))
+          angle = np.degrees(np.arcsin(horiz_dist/(depth)))
           cv2.putText(image, f"{angle:.2f} degrees", midpoint2, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 101, 255), 2)
-          cv2.putText(image, f"{depth_distance:.2f} in (depth)", midpoint3, cv2.FONT_HERSHEY_COMPLEX, 1, (255,0, 255), 2)
-          # print(f"{angle} degrees")
+          cv2.putText(image, f"{depth:.2f} in (depth)", midpoint3, cv2.FONT_HERSHEY_COMPLEX, 1, (255,0, 255), 2)
           #Step 1 starts here
-          if idDict.get(markerId) == "F2": 
+          if idDict.get(markerId) == "R2": ##TODO: Add B2, L2, F2 in the future
+            #Step 1 is over. Start Step 2: move forward until tag out of sight. "
             if angle <= angle_zero:
-              # print("Step 1 is over. Start Step 2: move forward until tag out of sight. ")
               motor.go_forward()
-              step1_done = True
+              Steps_Done.one = True
             else:
-              # Step 1 - print("Keep rotating till we reach center of middle tag")
+              # Step 1 - "Keep rotating till we reach center of middle tag"
               if direction_of_tag(corner[0],image) == "right":
                 motor.turn_left()
               else: 
@@ -122,25 +116,15 @@ while True:
           else:
             #All of this is Step 1 
             if idDict.get(markerId) not in visited: 
-              if idDict.get(markerId) is not None:
-                if(angle <= angle_zero):
-                  # print("aligned with the center of " + idDict.get(markerId))
-                  visited.append(idDict.get(markerId))
-                else:
-                  # print("keep rotating to align with center of " + idDict.get(markerId))
-                  if direction_of_tag(corner[0],image) == "right":
-                    motor.turn_left()
-                  else:
-                    motor_turn_right()
+              if(angle <= angle_zero):
+                visited.append(idDict.get(markerId))
               else:
-                # print("Impossible case. All april tags should be of 36h11 family. ")
-                pass
-            else:
-              # print("We have seen this tag before. Do not rotate.")
-              # print(visited)
-              pass
+                if direction_of_tag(corner[0],image) == "right":
+                  motor.turn_left()
+                else:
+                  motor.turn_right()
           #Step 3 + TODO: test how much turn_right and turn_left turns with our current robot + assume turn_right/left is 10 degrees
-          if step2_done: 
+          if Steps_Done.two: 
             num_calls = angle // 10
             if direction_of_tag(corner[0],image) == "right":
               for i in range(num_calls):
@@ -148,20 +132,18 @@ while True:
             else: 
               for i in range(num_calls):
                 motor.turn_left()
-            step3_done = True
+            Steps_Done.three = True
           
-          #step 4: reverse by 30 inches? 
-          if step3_done: 
-            if (depth_distance < 30):
+          #step 4: reverse by 30 inches 
+          if Steps_Done.three: 
+            if (depth < 30):
               motor.reverse()
-            step4_done = True
-         
+            Steps_Done.four = True
 
-
-    if step4_done == True:
+    if Steps_Done.four == True:
       break
              
-    cv2.imshow("yolo", image)
+    cv2.imshow("Camera", image)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
       break
