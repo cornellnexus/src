0ad4f2d095516bb2bcd3dc5eedcf3ec4c42cf73e
Amber Li <li.amber235@gmail.com>
2023-04-15 17:47:01 -0400

    Made edits based on PR comments including, changing the dictionary pins to an enum and making the list of broken pins a set instead of a list, as well as added comments for documentation

--- electrical/breakbeam.py
+++ electrical/breakbeam.py
@@ -1,46 +1,79 @@
 import RPi.GPIO as GPIO
 import time
+from enum import Enum
 
 class Breakbeam:
-    ACCEPTABLE_TIME = 50 # in seconds
-    BEAM_PINS = {17:'50%_1', 22:'50%_2', 23:'100%_1', 24:'100%_2'}
+    ACCEPTABLE_TIME = 50 # time to wait to confirm a full break
+    class BEAM_PINS(Enum):
+        HALF1 = 17
+        HALF2 = 22
+        FULL1 = 23
+        FULL2 = 24
+    
 
     def __init__(self):
-        for BEAM_PIN in Breakbeam.BEAM_PINS.keys():
+        """
+        Initializes each sensor to detect changes. Initializes beam_broken to False.
+        Creates an empty list for the beams that are broken.
+        """
+        for BEAM_PIN in Breakbeam.BEAM_PINS:
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             GPIO.add_event_detect(BEAM_PIN, GPIO.BOTH, callback=self.break_beam_callback)
-            print(self.timer(BEAM_PIN))
         self.beam_broken = False
-        self.blocked_sensors = []
+        self.blocked_sensors = set()
     
     def break_beam_callback(self, channel):
-        print(not GPIO.input(channel), channel)
+        """
+        Returns True if the beam at channel has been broken.
+        Parameters: channel - the pin used by this sensor
+        """
         self.beam_broken = not GPIO.input(channel)
-        if not GPIO.input(channel):
+        if self.beam_broken:
             self.timer(channel)
+            self.check_half()
+            self.check_full()
+        # print("50%: " + self.check_half())
+        # print("100%: " + self.check_full())
         return self.beam_broken
     
     def timer(self, channel):
+        """
+        Returns the pin number of this sensor.
+        Gets the current time and calculates the time that has passed, then compares
+        this to the ACCEPTABLE_TIME to determine whether the broken beam was just
+        tripped once or if it is fully broken. If fully broken, add it to the set
+        blocked_sensors.
+        Parameters: channel - the pin used by this sensor
+        """
         start = time.time()
-        while time.time() - start < Breakbeam.ACCEPTABLE_TIME:
-            if GPIO.input(channel) == False:
-                print('false reading')
-                if Breakbeam.BEAM_PINS[channel] in self.blocked_sensors: #it doesnt log duplicate values
-                    self.blocked_sensors.remove(Breakbeam.BEAM_PINS[channel])
-                break
+        # loops until ACCEPTABLE_TIME and then goes into the else statement once
+        while time.time() - start > Breakbeam.ACCEPTABLE_TIME:
+            pass
         else:
-            print('Time passed was ', time.time() - start, 'seconds')
-            self.blocked_sensors.append(Breakbeam.BEAM_PINS[channel])
-            self.check_full()
-            return channel
+            self.blocked_sensors.add(Breakbeam.BEAM_PINS(channel).name)
+            # return channel (for testing)
+        
+    def check_half(self):
+        """
+        Returns whether the 50% sensors are in the set of fully blocked sensors
+        """
+        if len(self.blocked_sensors) < 4 and Breakbeam.BEAM_PINS(17).name in self.blocked_sensors and Breakbeam.BEAM_PINS(22).name in self.blocked_sensors:
+            # bucket has been 50% filled
+            return True
+        else:
+            return False
         
     def check_full(self):
-        if len(self.blocked_sensors) == 2 and '50%_1' in self.blocked_sensors and '50%_2' in self.blocked_sensors:
-            return 'it was the 50 layer'
-        
+        """
+        Returns whether the 100% sensors are in the set of fully blocked sensors.
+        """
         if len(self.blocked_sensors) == 4:
-            return 'its full'
+            # bucket has been 100% filled
+            return True
+        else:
+            return False
+        
 
 # Allows to exit program and clean up Pi for testing
 message = input("Press enter to quit\n\n")
