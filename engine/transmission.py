238ee3cc6e9b5fdc18ec38c5fe001af54b1df649
Samantha Zhang <sjz46@cornell.edu>
2023-04-19 00:43:47 -0400

    add spec for transmission function

--- engine/transmission.py
+++ engine/transmission.py
@@ -10,10 +10,20 @@ logging.basicConfig(format=format, level=logging.INFO,
 # ser = serial.Serial("/dev/ttyS0", 57600) # Uncomment for RPI to GUI test
 # ser = serial.Serial("/dev/cu.usbserial-017543DC", 57600) # RPI_GUI_TEST
 def send_packet_to_gui(name, is_sim, robot_state, database):
+    """
+    Composes a data packet representing attributes of the current robot 
+    (and in the future mission state) and transmits the data for the GUI. Currently,
+    assumes there is a GUI listening for data packets.
 
+    Arguments:
+        name: int identifier for the thread
+        is_sim: boolean flag on whether this is a simulation
+        robot_state: Robot State object 
+        database: Database object # TODO replace
+    """
     logging.info("Thread %s: starting", name)
     while robot_state.phase != Phase.COMPLETE:
-        packet = database.make_packet()
+        packet = database.make_packet() # TODO: Replace implementation
         if not is_sim:
             # Sending data packet to gui from rpi
             cast_data = bytes(packet, encoding = 'utf-8') 
