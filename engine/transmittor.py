7d9617b3a4cf3fe94dbd45cb75335624d98944de
Aayush Agnihotri <aayush.agni1@gmail.com>
2023-04-24 17:57:31 -0400

    Creating transmittor.py

---
+++ engine/transmittor.py
@@ -0,0 +1,15 @@
+class Transmittor:
+    """
+    A class that will transmit the current robot and mission states to the GUI
+    """
+    
+    """
+    Call this function to transmit information to the GUI
+    """
+    def transmit_to_gui(self):
+        self.gather_info()
+    
+    def gather_info(self):
+        pass
+    
+    
