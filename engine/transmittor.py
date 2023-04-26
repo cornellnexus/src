14ee17ee8a5a6aa52ca7502de306b8798a85a4c0
Samantha Zhang <sjz46@cornell.edu>
2023-04-26 15:11:21 -0400

    initialize transmittor + some comments

--- engine/transmittor.py
+++ engine/transmittor.py
@@ -3,13 +3,34 @@ class Transmittor:
     A class that will transmit the current robot and mission states to the GUI
     """
     
-    """
-    Call this function to transmit information to the GUI
-    """
+    def __init__(self, robot_state, mission_state):
+        """
+        Store references to states. 
+
+        Parameters:
+            robot_state: an instance encapsulating conditions, measurements, etc. (i.e. all data) for a Robot instance
+            mission_state: an instance encapsulating conditions, measurements, etc. (i.e. all data) for a Mission instance
+        """
+        self.robot_state = robot_state
+        self.mission_state = mission_state
+                 
     def transmit_to_gui(self):
+        """
+        Call this function to transmit information to the GUI
+        """        
         self.gather_info()
     
     def gather_info(self):
+        """
+        Returns an encoded dictionary, containing the data across states.
+        """
+
+        # Create dictionary of information
+        # Cast to string and encode
+
+        # Information that we want:
+        # https://docs.google.com/document/d/15H-kN6vgFRpZ-XSjbv7ulJhqrt_15Kya2FYC6WzGCtI/edit
+
         pass
     
     
